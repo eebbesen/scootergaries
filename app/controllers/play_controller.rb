c81e1c3e84b1049fa88e6c52005d757be8b51b87
eebbesen <eebbesen.git@gmail.com>
2020-08-19 10:44:50 -0500

    rubocop

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,14 +1,11 @@
+# frozen_string_literal: true
+
 class PlayController < ApplicationController
   def join
     @game = Game.where(locator: params[:locator]).first
 
     if @game
-      @player = GamePlayer.where(
-        session_id: session_id,
-        game_id: @game.id
-      ).first_or_create
-
-      @player.save! unless @player.persisted?
+      player_check
 
       if @player.name
         render 'play'
@@ -22,15 +19,29 @@ class PlayController < ApplicationController
   def play
     @game = Game.find(params[:game_id])
     @player = GamePlayer.find(params[:player_id])
-    unless @player.name
-      @player.name = params[:name]
-      @player.save!
-    end
+
+    return if @player.name
+
+    @player.name = params[:name]
+    @player.save!
   end
+
+  private
 
   # for testing -- we don't have users login so this is not set by devise
   # but overridden in the system test
   def session_id
     session.id.to_s
+  end
+
+  def player_check
+    @player = GamePlayer.where(
+      session_id: session_id,
+      game_id: @game.id
+    ).first_or_create
+
+    @player.save! unless @player.persisted?
+
+    @player
   end
 end
